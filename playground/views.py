1943e0a7ed782440fe3651d4324c73e393d06605
Also known as (AK) <157801128+A-Khandan@users.noreply.github.com>
2024-11-21 12:08:16 +0000

    added media, bags and shoes, changed url.py and db, edited views.py for supporting media

--- playground/views.py
+++ playground/views.py
@@ -1,11 +1,19 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from item.models import Category, Item
 # This is a request handler
 # create view functions here
 # view function takes a request and sends a response to that
 
 def say_hello(request):
-    return render(request, 'hello.html', {'name': 'AK'})
+    items = Item.objects.filter(is_sold=False)[0:6]
+    categories = Category.objects.all()
+
+    return render(request, 'hello.html', {
+            'categories': categories,
+            'items': items,
+            'name': 'AK',
+        })
 
 def contact(request):
     return render(request, 'contact.html')
